fc23ed898d849781b642ee2d55f0efa525031372
reapdatofficial <125034206+reapdatofficial@users.noreply.github.com>
2024-01-23 04:56:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask
-
+from weasyprint import HTML
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello World, this is mani test!'
+
+
+HTML('https://render-rajkapdia-test.onrender.com').write_pdf('output.pdf')
 
 if __name__ == '__main__':
     app.run()
